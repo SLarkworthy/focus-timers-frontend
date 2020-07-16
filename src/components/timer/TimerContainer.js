c3754985c3ffe35c00937199ea90ad8ba30a5ccf
SLarkworthy <Sarah.Larkworthy@gmail.com>
2020-07-16 18:42:17 -0400

    fixed timer logic

--- src/components/timer/TimerContainer.js
+++ src/components/timer/TimerContainer.js
@@ -1,120 +1,87 @@
 import React from 'react';
 import Timer from 'react-compound-timer';
 import sound from '../../assets/alarm_sound.mp3';
-import Modal from '../UI/Modal';
-import TimerModal  from './TimerModal';
+import classes from './TimerContainer.module.css'
 
 class TimerContainer extends React.Component {
     constructor(props) {
         super(props);
-        this.userTimerWork = React.createRef();
-
-        this.state = {
-            showEdit: false,
-            workMode: true
-        }
+        this.userTimer = React.createRef();
       }
 
-    handleWorkCallback = () => {
-        this.userTimerWork.current.pause();
+    timerStyle = () => {
+        if (this.props.workMode) {
+            return {border: "2px solid red"} 
+        } else {
+            return{border: "2px solid blue"}
+        }
+    }
+
+    startCycle = start => {
+        start();
+    }
+
+    resetCycle = (reset, pause) => {
+        if (this.props.workMode) {
+            reset();
+            pause();
+        } else {
+            this.props.changeMode();
+            reset();
+            pause();
+        }
+    }
+
+    skipToBreak = (reset, pause) => {
+        if (this.props.workMode) {
+            this.props.changeMode();
+            reset();
+            pause();
+        } 
+    }
+
+    handleCallback = () => {
+        this.props.changeMode();
+        this.userTimer.current.reset();
         let audio = new Audio(sound);
         audio.play();
+        this.userTimer.current.start();
     }
-
-    handleBreakCallback = () => {
-        this.userTimerWork.current.reset();
-        let audio = new Audio(sound);
-        audio.play();
-    }
-
-    setStartTime = () => {
-        //dont forget!! break time plus work time!!
-        return 60 * 60 * 1000 * this.props.timer.work_time_hours + 60 * 1000 * this.props.timer.work_time_minutes + 60 * 60 * 1000 * this.props.timer.break_time_hours + 60 * 1000 * this.props.timer.break_time_minutes;
-    }
-
-    setBreakTime = () => {
-        return 60 * 60 * 1000 * this.props.timer.break_time_hours + 60 * 1000 * this.props.timer.break_time_minutes;
-    }
-    
 
     render() {
     return (
-        <>
-        <Modal>
-            <TimerModal 
-                workMode={this.state.workMode}/>
-        </Modal>
-        <Timer
-            ref={this.userTimerWork}
-            initialTime={this.setStartTime()}
+        <Timer 
+            key={this.props.timeToMiliseconds}
+            formatValue={(value) => `${(value < 10 ? `0${value}` : value)}`}
+            ref={this.userTimer}
+            initialTime={this.props.timeToMiliseconds}
             startImmediately={false}
             direction="backward"
             onStart={() => console.log('onStart hook')}
             onStop={() => console.log('onStop hook')}
             checkpoints={[
                 {
-                    time: this.setBreakTime(),
-                    callback: this.handleWorkCallback
-                },
-                {
-                    time: 0,
-                    callback:  this.handleBreakCallback
+                    time: 999,
+                    callback:  this.handleCallback
                 }
             ]}
         >
             {({ start, resume, pause, reset }) => (
             <>
-            <div>
-                <Timer.Hours /> hours
-                <Timer.Minutes /> minutes
-                <Timer.Seconds /> seconds
-            </div>
-            <br />
-            <div>
-                <button onClick={start}>Start</button>
+            <div className={classes.Buttons}>
+                <button onClick={() => this.startCycle(start)}>Start Cycle</button>
                 <button onClick={pause}>Pause</button>
                 <button onClick={resume}>Resume</button>
-                <button onClick={reset}>Reset</button>
+                <button onClick={() => this.resetCycle(reset, pause)}>Reset Cycle</button>
+                {this.props.workMode ? <button onClick={() => this.skipToBreak(reset, pause)}>Skip to Break</button> : null}
             </div>
+            <div className={classes.Box} style={this.timerStyle()}>
+                <Timer.Hours />:<Timer.Minutes />:<Timer.Seconds />
+            </div>
+            <br />
             </>
             )}
         </Timer>
-        <Timer
-            ref={this.userTimerBreak}
-            initialTime={this.setStartTime()}
-            startImmediately={false}
-            direction="backward"
-            onStart={() => console.log('onStart hook')}
-            onStop={() => console.log('onStop hook')}
-            checkpoints={[
-                {
-                    time: this.setBreakTime(),
-                    callback: this.handleWorkCallback
-                },
-                {
-                    time: 0,
-                    callback:  this.handleBreakCallback
-                }
-            ]}
-        >
-            {({ start, resume, pause, reset }) => (
-            <>
-            <div>
-                <Timer.Hours /> hours
-                <Timer.Minutes /> minutes
-                <Timer.Seconds /> seconds
-            </div>
-            <br />
-            <div>
-                <button onClick={start}>Start</button>
-                <button onClick={pause}>Pause</button>
-                <button onClick={resume}>Resume</button>
-                <button onClick={reset}>Reset</button>
-            </div>
-            </>
-            )}
-        </Timer>
-        </>
     )
 }}
 
