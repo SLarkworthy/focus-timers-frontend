df174910f0f9911bdf833d9b65e40d4dc55e8444
SLarkworthy <Sarah.Larkworthy@gmail.com>
2020-07-17 17:56:14 -0400

    added example timer to homepage

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -1,9 +1,12 @@
 import React from "react";
 import generalClasses from '../../../src/App.module.css';
+import classses from '../activity/ActivityContainer.module.css'
+import TimerState from '../timer/TimerState'
 
 
 const homepage = props => {
     return (
+        <>
         <div className={generalClasses.Card}>
             <h1>Welcome to Focus Buddy!</h1>
             <p>This app is designed to let you create custom timed work and break cycles for any activity. </p>
@@ -11,6 +14,14 @@ const homepage = props => {
             <br />
             <p style={{color: "purple"}}>Random affirmation: <em>{props.quote}</em></p>
         </div>
+        <div className={generalClasses.Card}>
+            <h2>Here is an example timer!</h2>
+            <p>This example timer works in cycles of 20 seconds of work, 20 seconds of break. Make an account to customize your own!</p>
+            <div className={classses.Timer}>
+                <TimerState practiceMode={true} />
+            </div>
+        </div>
+        </>
     )
 }
 
--- src/components/timer/TimerState.js
+++ src/components/timer/TimerState.js
@@ -11,6 +11,9 @@ class TimerState extends Component {
     }
 
     timeToMiliseconds = () => {
+        if (this.props.practiceMode) {
+            return 20 * 1000 + 999
+        }
         if (this.state.workMode) {
             return 60 * 60 * 1000 * this.props.timer.work_time_hours + 60 * 1000 * this.props.timer.work_time_minutes + 999
         } else {
